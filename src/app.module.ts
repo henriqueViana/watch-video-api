9d8515420809ed9a19f594102deb10cff7a09d73
Henrique Viana <henriquehvisa@gmail.com>
2025-07-13 10:01:36 -0300

    feat: set global modules in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { KafkaModule } from './infra/kafka/kafka.module';
+import { ConfigModule } from '@nestjs/config';
+import { S3Module } from './infra/s3/s3.module';
+import { GetVideoStreamUseCase } from './application/use-case/get-video-stream';
+import { VideoController } from './interfaces/controllers/video.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    KafkaModule,
+    S3Module
+  ],
+  controllers: [AppController, VideoController],
+  providers: [AppService, GetVideoStreamUseCase],
 })
 export class AppModule {}
