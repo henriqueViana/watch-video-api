1510ebd23b48cf4e895d1e3058dc51d66545c60d
Henrique Viana <henriquehvisa@gmail.com>
2025-07-13 10:00:32 -0300

    feat: set global prefix with version and enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('v1/api');
+  app.enableCors({
+    origin: '*', // Adjust this to your needs
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  }); 
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
